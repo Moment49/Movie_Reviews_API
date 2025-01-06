02885db428e63c201ec44d63aade479d1b932047
Moment49 <ibenachoelvis49@gmail.com>
2025-01-06 05:50:16 +0100

    first commit

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 
 # Create your models here.
+class UserManager(BaseUserManager):
+    def create_user():
+        ...
+    def create_superuser():
+        ...
+
+class CustomUser(AbstractUser):
+    ...
+
+class UserProfile(models.Model):
+    ...
