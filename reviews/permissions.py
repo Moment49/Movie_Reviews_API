259f0386e4609a7036fe605e4c2a4138558fcf89
Moment49 <ibenachoelvis49@gmail.com>
2025-01-08 04:01:46 +0100

    worked on the users, reviews features

---
+++ reviews/permissions.py
@@ -0,0 +1,10 @@
+from rest_framework.permissions import BasePermission
+from rest_framework.validators import ValidationError
+from rest_framework import status
+
+class CustomPermission(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if obj.user == request.user:
+            return True
+        else:
+            raise ValidationError({"message":"Sorry!! You can't update or delete a review that is not yours"})
